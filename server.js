e9825e1db81be948d0f75fea790f11837d716f9d
Joshua Bell <josh@bellaliant.net>
2018-09-13 12:40:34 -0300

    added two functions that allow the server to know how many ships have beenShot

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var server = http.Server(app);
 var io = socketIO(server);
 
 var map = gameMap.generateJsonMap();
+var numShipsShot = 0;
 
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
@@ -23,9 +24,58 @@ io.on('connection', function(socket) {
 	io.sockets.emit('map', map);
 	socket.on('updateMap', function(globalMap){
 		map = globalMap;
+		blastFireWork(determineShipsHit(map));
 		io.sockets.emit('map', map);
 	});
 });
+
+
+function determineShipsHit(newMap){ 
+ var counter = 0;
+ for( var i = 0; i < newMap.buttons.length; i++){
+ 	if(newMap.buttons[i].hasBeenShot == true && newMap.buttons[i].isTarget == true){
+ 	  counter++;
+ 	} 
+ }
+ return counter;
+}
+
+function blastFireWork(counter){
+
+  if(counter > numShipsShot){
+	switch (counter) {
+		case 0:
+		break;
+		
+		case 1:
+		numShipsShot = 1;
+		// Replace with pin1 high
+		break;
+		
+		case 2:
+		numShipsShot = 2;
+		//replace with pin2 high
+		break;
+		
+		case 3:
+		numShipsShot = 3;
+		//replace with pin3 high
+		break;
+		
+		case 4:
+		numShipsShot = 4;
+		//replace with pin4 high
+		break;
+		
+		case 5:
+		numShipsShot = 5;
+		//replace with pin5 high
+		break;
+	
+	}
+  }
+  
+}
 
 // Starts the server.
 server.listen(5000, function() {
